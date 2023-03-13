d079c46292a86ba12ddf923b3613bc20b44960f3
EliasAlmen <almen.elias@gmail.com>
2023-03-13 19:42:29 +0100

    Before redesign DB

---
+++ EC05_C-sharp-Case-mgmt-wpf/MVVM/Models/Entities/CaseStatusEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC05_C_sharp_Case_mgmt_wpf.MVVM.Models.Entities
+{
+    public class CaseStatusEntity
+    {
+        public int Id { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ EC05_C-sharp-Case-mgmt-wpf/MVVM/Models/Entities/OwnerEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC05_C_sharp_Case_mgmt_wpf.MVVM.Models.Entities
+{
+    internal class OwnerEntity
+    {
+    }
+}
